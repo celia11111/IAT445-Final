e4e8dfda4072c2bf87033f07fe2f4f3ecd886f06
Unknown <sherryshiyiwang@gmail.com>
2019-12-03 20:28:45 -0800

    semi-final

---
+++ Assets/CheckPuzzle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPuzzle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public changeScene changeS;
+    public BoxCollider box;
+    public CheckKey r,g,b;
+    public Animator anime;
+    void Start()
+    {
+        StartCoroutine(doStuff());
+    }
+    IEnumerator doStuff()
+    {
+        yield return new WaitUntil(()=> r.startMovement && g.startMovement && b.startMovement);
+        anime.SetBool("start", true);
+        yield return new WaitForSeconds(5);
+        box.enabled = true;
+        changeS.enabled = true;
+    }
+}
